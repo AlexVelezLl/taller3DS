a9307f5f4cf1db443c63355c5107f604624545ce
AlvaradoAlexis <eaalvara@espol.edu.ec>
2020-06-24 10:14:42 -0500

    Agregar clase PagoPaypal

---
+++ TallerSOLID/src/taller/PagoPaypal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+/**
+ *
+ * @author Alexis
+ */
+public class PagoPaypal implements Pago{
+    
+    @Override
+    public void realizarCobro(double monto) {
+     //aquí se realiza el cobro   
+    }
+    
+}
