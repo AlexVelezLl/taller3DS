1224d70b6dfbeae9d3a129af8aeeffd08300e7d1
caloja1014 <caloja@espol.edu.ec>
2020-06-24 12:36:53 -0500

    Creacion de la clase compra

---
+++ TallerSOLID/src/taller/Compra.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+import java.util.List;
+
+/**
+ *
+ * @author CLOJA
+ */
+public class Compra {
+    private Pago pago;
+    private List articulos;
+    public Compra (Pago pago){
+        
+    }
+    
+    public void agregarArticulo(Articulo articulo){
+    }
+    public void removerArticulo(Articulo articulo){
+        
+    }
+}
+class Articulo{
+    
+}
