aba92c1b0a71d45e3b236b35ae6041a5a19ff9d0
caloja1014 <caloja@espol.edu.ec>
2020-06-24 11:21:34 -0500

    Creacion del medio de notificacion telegram

---
+++ TallerSOLID/src/taller/Telegram.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+/**
+ *
+ * @author CLOJA
+ */
+public class Telegram implements Notificacion {
+
+    @Override
+    public void notificar(Pago pago) {
+        //Enviar email
+    }
+}
