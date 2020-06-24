bde05d1e4c6aaf4cb111189e82705beda9f8b649
JaimePizarro <japibaro@espol.edu.ec>
2020-06-24 10:39:49 -0500

    Creacion interface Factura, SRP

---
+++ TallerSOLID/src/taller/FacturaImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+/**
+ *
+ * @author PC
+ */
+public class FacturaImpl implements Factura{
+    
+    //atributes
+    
+    @Override
+    public void calcularImpuestosFactura() {
+        //Implementation
+    }
+
+    @Override
+    public void generarFactura() {
+        //Implementation
+    }
+    
+}
